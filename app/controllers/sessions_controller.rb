7e12859ff157b705787539291d9b3c0cdfd01665
hegetech <yoshiroh.miyata@gmail.com>
2016-10-05 11:22:41 +0000

    9.2.2までremember me box 除く testエラー修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
      user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      log_in user
+      log_in user     # 永続的クッキー
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination' 
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -8,7 +8,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, session: { email: "", password: "" }
+    post login_path, params:{session: { email: "", password: "" }}
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
@@ -29,6 +29,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
