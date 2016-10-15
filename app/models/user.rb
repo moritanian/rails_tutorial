8f1192a950e9eb92322fbdc321cf1d2dcb583589
hegetech <yoshiroh.miyata@gmail.com>
2016-10-15 06:37:24 +0000

    revert microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many :microposts, dependent: :destroy
     attr_accessor :remember_token, :activation_token, :reset_token
+   # before_save   :downcase_email
     before_create :create_activation_digest
     attr_accessor :remember_token
     before_save { self.email = email.downcase } # db保存前にlowercase に変更
@@ -71,12 +71,6 @@ class User < ApplicationRecord
   # Returns true if a password reset has expired.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
-  end
-  
-  # Defines a proto-feed.
-  # See "Following users" for the full implementation.
-  def feed
-    Micropost.where("user_id = ?", id)
   end
   
   private 
